22f88aa2900c3f3ed524b3c32162a2db8ae83a29
Tran Minh Man <96435164+TranMinhMan040702@users.noreply.github.com>
2023-01-01 22:29:51 +0700

    RemoveElement

---
+++ src/leetcode/RemoveElement.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+public class RemoveElement {
+	public static void main(String[] args) {
+		int[] nums = {0,1,2,2,3,0,4,2};
+		int val = 2;
+		System.out.println(removeElement(nums, val));
+	}
+    public static int removeElement(int[] nums, int val) {
+      
+        int index = 0;
+        for (int i = 0; i < nums.length; i++) {
+        	if (nums[i] != val) {
+        		nums[index] = nums[i];
+        		index++;
+        	} 
+        }
+        
+        for (Integer item : nums) {
+        	System.out.println(item);
+        }
+        
+        return index;
+    }
+}
